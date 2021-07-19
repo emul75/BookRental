abd53ff8465395397d8cad43dfb3b7c726b6ebca
emul75 <emilx.75@gmail.com>
2021-07-19 17:10:02 +0200

    Added Book entity and BookRentalDbContext.

---
+++ BookRental/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BookRental.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string ClientName { get; set; }
+        public DateTime Published { get; set; }
+        public DateTime Rented { get; set; }
+        public DateTime Returned { get; set; }
+        public bool RentalStatus { get; set; }
+        
+    }
+}
---
+++ BookRental/Entities/BookRentalDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookRental.Entities
+{
+    public class BookRentalDbContext : DbContext
+
+    {
+        private string _connectionString =
+            "Server=(localdb)\\mssqllocaldb;Database=TicTacToeDb;Trusted_Connection=True;";
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .Property(b => b.Title)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            modelBuilder.Entity<Book>()
+                .Property(b => b.Author)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            modelBuilder.Entity<Book>()
+                .Property(b => b.Category)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            modelBuilder.Entity<Book>()
+                .Property(b => b.Published)
+                .IsRequired();
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
